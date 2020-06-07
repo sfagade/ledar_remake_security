d84154153a439c422062cf81288ddde158e52a63
samsonfagade <sfagade@gmail.com>
2020-06-07 08:26:36 +0100

    Added project to gateway service Created startup sql script for initial data creation

---
+++ src/main/resources/postgres_startup_script.sql
@@ -0,0 +1,18 @@
+INSERT INTO public.authentication_roles(
+        authentication_role_id, created, modified, created_by, is_restricted, role_description, role_name, role_order, role_type, url_mapping)
+        VALUES (1, '2018-03-08 11:9:40', '2018-03-08 11:9:40', 0, false, 'Administrator', 'ADMINISTRATOR', 1, 1, NULL);
+INSERT INTO public.authentication_roles(
+        authentication_role_id, created, modified, created_by, is_restricted, role_description, role_name, role_order, role_type, url_mapping)
+        VALUES (2, '2018-03-08 11:11:09', '2018-03-08 11:11:09', 0, false, 'REPORT ADMINISTRATOR', 'REPORT ADMINISTRATOR', 2, 1, 1);
+INSERT INTO public.authentication_roles(
+        authentication_role_id, created, modified, created_by, is_restricted, role_description, role_name, role_order, role_type, url_mapping)
+        VALUES (3, '2018-03-08 11:11:09', '2018-03-08 11:11:09', 0, false, 'Staff Administrator', 'STAFF', 2, 1, NULL);
+INSERT INTO public.authentication_roles(
+        authentication_role_id, created, modified, created_by, is_restricted, role_description, role_name, role_order, role_type, url_mapping)
+        VALUES (4, '2018-03-08 11:11:11', '2018-03-08 11:11:11', NULL, false, 'PARTNER', 'PARTNER ADMINISTRATOR', 4, NULL, NULL);
+INSERT INTO public.authentication_roles(
+        authentication_role_id, created, modified, created_by, is_restricted, role_description, role_name, role_order, role_type, url_mapping)
+        VALUES (5, '2018-03-08 11:11:11', '2018-03-08 11:11:11', NULL, false, 'SALES REPRESENTATIVE', 'SALES REPRESENTATIVE', 4, NULL, NULL);
+INSERT INTO public.authentication_roles(
+        authentication_role_id, created, modified, created_by, is_restricted, role_description, role_name, role_order, role_type, url_mapping)
+        VALUES (6, '2018-03-08 11:11:11', '2018-03-08 11:11:11', NULL, false, 'CUSTOMER', 'CUSTOMER', 3, NULL, NULL);
