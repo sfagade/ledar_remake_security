44dd90522998e26baa5130200c10576a8adbae60
samsonfagade <sfagade@gmail.com>
2020-07-28 21:03:36 +0100

    Added maven dependencies for making application eureka client added eureka client annotation to app start class

--- src/main/java/org/tsp/projects/ledar/security/LedarSecurityApplication.java
+++ src/main/java/org/tsp/projects/ledar/security/LedarSecurityApplication.java
@@ -2,8 +2,10 @@ package org.tsp.projects.ledar.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LedarSecurityApplication {
 
 	public static void main(String[] args) {
